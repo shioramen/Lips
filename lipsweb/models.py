b5eb8a813c498fa0837d7d712c26d0b1dda7309d
shioramen <esther.t0000@gmail.com>
2017-09-05 15:08:18 -0700

    removed uncalled functions

--- lipsweb/models.py
+++ lipsweb/models.py
@@ -6,13 +6,6 @@ from django.db import models
 class Submission(CMSPlugin):
     content = models.TextField(default='')
     upvoteCount = models.PositiveIntegerField(default=0)
-
-    def countSubmission(self):
-        return count(self.content)
-
-    def countUpvote(self):
-        return self.upvoteCount
-
-
+    
 class AboutLips(CMSPlugin):
     text = models.TextField(default='')
